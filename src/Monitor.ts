4deebb7196f35803c826d67a60fd9b400f9f9a66
Yury Chetyrko <ychetyrko@gmail.com>
2019-02-21 22:54:55 +0300

    Monitor will not be used for maintaining the list of active transactions

--- src/Monitor.ts
+++ src/Monitor.ts
@@ -1,6 +1,5 @@
 import { Handle } from "./internal/z.index";
 import { state, Isolation } from "./Config";
-import { Reactronic } from "./Reactronic";
 
 @state
 export class Monitor {
@@ -8,12 +7,10 @@ export class Monitor {
 
   private _idle: boolean = true;
   private _volume: number = 0;
-  private _running: Array<Reactronic<any>> = [];
 
   readonly isolation: Isolation;
   get isIdle(): boolean { return this._idle; }
   get volume(): number { return this._volume; }
-  get running(): ReadonlyArray<Reactronic<any>> { return this._running; }
 
   constructor(name: string, isolation: Isolation = Isolation.StandaloneTransaction) {
     this.isolation = isolation;
