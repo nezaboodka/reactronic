cacd2110f5ea8440739bc588eded3653160b5f69
Yury Chetyrko <ychetyrko@gmail.com>
2019-08-30 10:12:34 +0300

    Unit tests cosmetics

--- tests/async.ts
+++ tests/async.ts
@@ -1,5 +1,5 @@
 ﻿import { all, Debug } from "../src/internal/z.index";
-import { stateful, transaction, cache, Renew, ReentrantCall, ApartFrom, Monitor, monitor, Transaction } from "../src/z.index";
+import { stateful, transaction, cache, Renew, Monitor, monitor, Transaction } from "../src/z.index";
 import { sleep } from "./common";
 
 export const actual: string[] = [];
@@ -33,7 +33,7 @@ export class DemoView {
     return result;
   }
 
-  @cache(Renew.Immediately, ReentrantCall.ExitWithError, ApartFrom.Parent)
+  @cache(Renew.Immediately)
   async print(): Promise<void> {
     let lines: string[] = await this.render();
     for (let x of lines) {
