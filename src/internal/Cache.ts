04a73c80c6e34d8e6dacf71ae63267a3a032eaf4
Yury Chetyrko <ychetyrko@gmail.com>
2019-09-01 13:07:46 +0300

    Cosmetics

--- src/internal/Cache.ts
+++ src/internal/Cache.ts
@@ -5,7 +5,7 @@ import { Config, Renew, ReentrantCall, ApartFrom } from "../Config";
 import { Transaction } from "../Transaction";
 import { Monitor } from "../Monitor";
 
-interface CacheCall {
+interface CachedCall {
   record: Record;
   cache: CachedResult;
   isUpToDate: boolean;
@@ -21,7 +21,7 @@ class CachedMethod extends ReactiveCache<any> {
   get error(): boolean { return this.read(true).cache.error; }
   invalidate(cause: string | undefined): boolean { return cause ? CachedResult.enforceInvalidation(this.read(false).cache, cause, 0) : false; }
   get isComputing(): boolean { return this.read(true).cache.started > 0; }
-  get isUpdating(): boolean { return this.read(true).cache.outdated.recomputation !== undefined; }
+  get isUpdating(): boolean { return this.read(true).cache.outdated.recaching !== undefined; }
 
   constructor(handle: Handle, member: PropertyKey, config: ConfigImpl) {
     super();
@@ -32,61 +32,60 @@ class CachedMethod extends ReactiveCache<any> {
   }
 
   recent(...args: any): any {
-    let cc = this.obtain(false, args);
-    if (cc.isUpToDate || cc.record.snapshot.completed)
-      Record.markViewed(cc.record, cc.cache.member);
-    else if (cc.record.prev.record !== Record.empty)
-      Record.markViewed(cc.record.prev.record, cc.cache.member);
-    return cc.cache.result;
+    let call: CachedCall = this.obtain(false, args);
+    if (call.isUpToDate || call.record.snapshot.completed)
+      Record.markViewed(call.record, call.cache.member);
+    else if (call.record.prev.record !== Record.empty)
+      Record.markViewed(call.record.prev.record, call.cache.member);
+    return call.cache.result;
   }
 
   get stamp(): number {
-    let cc = this.obtain();
-    let r = cc.isUpToDate ?  cc.record : cc.record.prev.record;
+    let call: CachedCall = this.obtain();
+    let r = call.isUpToDate ?  call.record : call.record.prev.record;
     if (r !== Record.empty)
-      Record.markViewed(r, cc.cache.member);
+      Record.markViewed(r, call.cache.member);
     return r.snapshot.timestamp;
   }
 
   get isInvalidated(): boolean {
-    let cc = this.obtain();
-    let result = cc.cache.isInvalidated();
+    let call: CachedCall = this.obtain();
+    let result = call.cache.isInvalidated();
     if (result)
-      Record.markViewed(cc.record, cc.cache.member);
-    else if (cc.record.prev.record !== Record.empty)
-      Record.markViewed(cc.record.prev.record, cc.cache.member);
+      Record.markViewed(call.record, call.cache.member);
+    else if (call.record.prev.record !== Record.empty)
+      Record.markViewed(call.record.prev.record, call.cache.member);
     // Record.markViewed(cc.record, cc.cache.member);
     return result;
   }
 
   invoke(...args: any[]): any {
-    let cc = this.obtain(true, args);
-    Record.markViewed(cc.record, cc.cache.member);
-    return cc.cache.ret;
+    let call: CachedCall = this.obtain(true, args);
+    Record.markViewed(call.record, call.cache.member);
+    return call.cache.ret;
   }
 
-  obtain(invoke?: boolean, args?: any[]): CacheCall {
-    let cc = this.read(false, args);
-    let cc2: CacheCall = cc;
-    if (!cc2.isHit) {
-      let c: CachedResult = cc.cache;
-      if (invoke !== undefined && (!c.outdated.recomputation || invoke)) {
-        let hint: string = (c.config.tracing >= 2 || Debug.verbosity >= 2) ? `${Hint.handle(this.handle)}.${c.member.toString()}${args && args.length > 0 ? `/${args[0]}` : ""}` : "recache";
-        let ret = Transaction.runAs<any>(hint, c.config.apart, c.config.tracing, (argsx: any[] | undefined): any => {
-          if (cc2.cache.tran.discarded())
-            cc2 = this.read(false, argsx); // re-read on retry
-          cc2 = this.recache(cc2.cache, argsx);
-          return cc2.cache.ret;
+  obtain(invoke?: boolean, args?: any[]): CachedCall {
+    let call: CachedCall = this.read(false, args);
+    if (!call.isHit) {
+      let c: CachedResult = call.cache;
+      if (invoke !== undefined && (!c.outdated.recaching || invoke)) {
+        const hint: string = (c.config.tracing >= 2 || Debug.verbosity >= 2) ? `${Hint.handle(this.handle)}.${c.member.toString()}${args && args.length > 0 ? `/${args[0]}` : ""}` : "recache";
+        const ret = Transaction.runAs<any>(hint, c.config.apart, c.config.tracing, (argsx: any[] | undefined): any => {
+          if (call.cache.tran.discarded())
+            call = this.read(false, argsx); // re-read on retry
+          call = this.recache(call.cache, argsx);
+          return call.cache.ret;
         }, args);
-        cc2.cache.ret = ret;
+        call.cache.ret = ret;
       }
     }
     else
-      if (Debug.verbosity >= 2) Debug.log("║", "  ==", `${Hint.record(cc.record)}.${cc2.cache.member.toString()} hits cache created by ${cc.cache.tran.hint}`);
-    return cc2;
+      if (Debug.verbosity >= 2) Debug.log("║", "  ==", `${Hint.record(call.record)}.${call.cache.member.toString()} hits cache created by ${call.cache.tran.hint}`);
+    return call;
   }
 
-  private read(markViewed: boolean, args?: any[]): CacheCall {
+  private read(markViewed: boolean, args?: any[]): CachedCall {
     let ctx = Snapshot.active();
     let member = this.blank.member;
     let r: Record = ctx.tryRead(this.handle);
@@ -101,7 +100,7 @@ class CachedMethod extends ReactiveCache<any> {
     return { cache: c, record: r, isUpToDate, isHit };
   }
 
-  private edit(): CacheCall {
+  private edit(): CachedCall {
     let ctx = Snapshot.active();
     let member = this.blank.member;
     let r: Record = ctx.edit(this.handle, member, RT_CACHE);
@@ -118,56 +117,56 @@ class CachedMethod extends ReactiveCache<any> {
     return { cache: c, record: r, isUpToDate, isHit: false };
   }
 
-  private recache(c: CachedResult, args: any[] | undefined): CacheCall {
-    const error = this.checkForReentrance(c);
-    let cc2 = this.edit();
-    let c2: CachedResult = cc2.cache;
-    let r2: Record = cc2.record;
-    let mon: Monitor | null = c.config.monitor;
+  private recache(prev: CachedResult, args: any[] | undefined): CachedCall {
+    const error = this.checkForReentrance(prev);
+    let call: CachedCall = this.edit();
+    let c: CachedResult = call.cache;
+    let r: Record = call.record;
+    let mon: Monitor | null = prev.config.monitor;
     if (!error)
-      c2.enter(r2, c, mon);
+      c.enter(r, prev, mon);
     try
     {
       if (args)
-        c2.args = args;
+        c.args = args;
       else
-        args = c2.args;
+        args = c.args;
       if (!error)
-        c2.ret = CachedResult.run<any>(c2, (...argsx: any[]): any => {
-          return c2.config.body.call(this.handle.proxy, ...argsx);
+        c.ret = CachedResult.run<any>(c, (...argsx: any[]): any => {
+          return c.config.body.call(this.handle.proxy, ...argsx);
         }, ...args);
       else
-        c2.ret = Promise.reject(error);
-      c2.outdated.timestamp = Number.MAX_SAFE_INTEGER;
+        c.ret = Promise.reject(error);
+      c.outdated.timestamp = Number.MAX_SAFE_INTEGER;
     }
     finally {
       if (!error)
-        c2.tryLeave(r2, c, mon);
+        c.tryLeave(r, prev, mon);
     }
-    cc2.isUpToDate = c2.started === 0;
-    return cc2;
+    call.isUpToDate = c.started === 0;
+    return call;
   }
 
   private checkForReentrance(c: CachedResult): Error | undefined {
     let result: Error | undefined = undefined;
-    const prev = c.outdated.recomputation;
+    const existing = c.outdated.recaching;
     const caller = Transaction.active;
-    if (prev)
+    if (existing)
       switch (c.config.reentrant) {
         case ReentrantCall.ExitWithError:
           throw new Error(`[E609] ${c.hint()} is not reentrant`);
         case ReentrantCall.WaitAndRestart:
-          result = new Error(`transaction will be restarted after t${prev.tran.id} (${prev.tran.hint})`);
-          caller.discard(result, prev.tran);
+          result = new Error(`transaction will be restarted after t${existing.tran.id} (${existing.tran.hint})`);
+          caller.discard(result, existing.tran);
           break;
         case ReentrantCall.DiscardPrevious:
-          result = new Error(`transaction will be restarted after t${prev.tran.id} (${prev.tran.hint})`);
-          prev.tran.discard();
-          caller.discard(result, prev.tran);
+          result = new Error(`transaction will be restarted after t${existing.tran.id} (${existing.tran.hint})`);
+          existing.tran.discard();
+          caller.discard(result, existing.tran);
           break;
         case ReentrantCall.DiscardPreviousNoWait:
-          prev.tran.discard();
-          c.outdated.recomputation = undefined;
+          existing.tran.discard();
+          c.outdated.recaching = undefined;
           break;
         case ReentrantCall.RunSimultaneously:
           break; // do nothing
@@ -204,7 +203,7 @@ export class CachedResult implements ICachedResult {
   result: any;
   error: any;
   started: number;
-  readonly outdated: { timestamp: number, recomputation: CachedResult | undefined };
+  readonly outdated: { timestamp: number, recaching: CachedResult | undefined };
   readonly observables: Map<PropertyKey, Set<Record>>;
   readonly hotObservables: Map<PropertyKey, Set<Record>>;
 
@@ -226,7 +225,7 @@ export class CachedResult implements ICachedResult {
     // this.ret = undefined;
     // this.error = undefined;
     this.started = 0;
-    this.outdated = { timestamp: 0, recomputation: undefined };
+    this.outdated = { timestamp: 0, recaching: undefined };
     this.observables = new Map<PropertyKey, Set<Record>>();
     this.hotObservables = new Map<PropertyKey, Set<Record>>();
   }
@@ -275,7 +274,7 @@ export class CachedResult implements ICachedResult {
   triggerRecache(timestamp: number, now: boolean, args?: any[]): void {
     if (now || this.config.latency === Renew.Immediately) {
       if (!this.error && (this.config.latency === Renew.NoCache ||
-          (timestamp >= this.outdated.timestamp && !this.outdated.recomputation))) {
+          (timestamp >= this.outdated.timestamp && !this.outdated.recaching))) {
         let proxy: any = Utils.get(this.record.data, RT_HANDLE).proxy;
         let trap: Function = Reflect.get(proxy, this.member, proxy);
         let cachedMethod: CachedMethod = Utils.get(trap, RT_CACHE);
@@ -429,8 +428,8 @@ export class CachedResult implements ICachedResult {
     if (this.config.tracing >= 3 || (this.config.tracing === 0 && Debug.verbosity >= 3)) Debug.log("║", "  ‾\\", `${Hint.record(r, true)}.${this.member.toString()} - enter`);
     this.started = Date.now();
     this.monitorEnter(mon);
-    if (!prev.outdated.recomputation)
-      prev.outdated.recomputation = this;
+    if (!prev.outdated.recaching)
+      prev.outdated.recaching = this;
   }
 
   tryLeave(r: Record, prev: CachedResult, mon: Monitor | null): void {
@@ -457,8 +456,8 @@ export class CachedResult implements ICachedResult {
   }
 
   private leave(r: Record, prev: CachedResult, mon: Monitor | null, op: string, message: string, highlight: string | undefined = undefined): void {
-    if (prev.outdated.recomputation === this)
-      prev.outdated.recomputation = undefined;
+    if (prev.outdated.recaching === this)
+      prev.outdated.recaching = undefined;
     this.monitorLeave(mon);
     const ms: number = Date.now() - this.started;
     this.started = 0;
