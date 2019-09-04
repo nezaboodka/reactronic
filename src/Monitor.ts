5697a1b7350fb8f5e9a25deab1d9be2794c64038
Yury Chetyrko <ychetyrko@gmail.com>
2019-09-04 16:21:06 +0300

    Cosmetics

--- src/Monitor.ts
+++ src/Monitor.ts
@@ -7,27 +7,27 @@ import { Transaction } from "./Transaction";
 export class Monitor {
   private _idle: boolean = true;
   private _counter: number = 0;
-  private _operations = new Set<Operation>();
+  private _workers = new Set<Worker>();
   readonly prolonged: boolean;
   readonly separate: SeparateFrom;
   get isIdle(): boolean { return this._idle; }
   get counter(): number { return this._counter; }
-  get operations(): ReadonlySet<Operation> { return this._operations; }
+  get workers(): ReadonlySet<Worker> { return this._workers; }
 
   constructor(prolonged: boolean = false, separate: SeparateFrom = SeparateFrom.All) {
     this.prolonged = prolonged;
     this.separate = separate;
   }
 
-  enter(op: Operation): void {
+  enter(worker: Worker): void {
     if (this._counter === 0)
       this._idle = false;
     this._counter++;
-    this._operations.add(op);
+    this._workers.add(worker);
   }
 
-  leave(op: Operation): void {
-    this._operations.delete(op);
+  leave(worker: Worker): void {
+    this._workers.delete(worker);
     this._counter--;
     if (this._counter === 0)
       this._idle = true;
@@ -38,7 +38,7 @@ export class Monitor {
   }
 }
 
-export interface Operation {
+export interface Worker {
   // hint(tranless?: boolean): string;
   readonly tran: Transaction;
   // readonly progress: number; // 0..100
--- src/z.index.ts
+++ src/z.index.ts
@@ -4,4 +4,4 @@ export { ReactiveCache, resultof } from "./internal/Cache";
 export { Config, Mode, Renew, ReentrantCall, SeparateFrom } from "./Config";
 export { stateful, stateless, transaction, cache, behavior, monitor, tracing, config } from "./Config.decorators";
 export { Transaction } from "./Transaction";
-export { Monitor, Operation } from "./Monitor";
+export { Monitor, Worker } from "./Monitor";
