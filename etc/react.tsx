e1502176e928b5b742f5d78b64f4e493fd86461b
Yury Chetyrko <ychetyrko@gmail.com>
2019-09-08 13:52:33 +0300

    React example

--- etc/react.tsx
+++ etc/react.tsx
@@ -4,19 +4,15 @@ import * as React from 'react';
 import { stateful, transaction, cache, behavior, cacheof,
   Renew, SeparateFrom, Transaction, Cache, Trace} from '../src/z.index';
 
-let renderings: number = 0;
-
 export function reactiveRender(render: (revision: number) => JSX.Element, trace?: Partial<Trace>, tran?: Transaction): JSX.Element {
   const restore = trace ? Cache.pushTrace(trace) : Cache.trace;
   try {
-    renderings++;
     const [jsx] = React.useState(() => createJsx(trace));
     const [revision, refresh] = React.useState(0);
     React.useEffect(unmountEffect(jsx), []);
     return jsx.render(revision, render, refresh, tran);
   }
   finally {
-    renderings--;
     Cache.trace = restore;
   }
 }
@@ -37,12 +33,8 @@ class Jsx {
 
   @cache @behavior(Renew.AsyncBatch)
   trigger(nextRevision: number, refresh: (nextRevision: number) => void): void {
-    if (cacheof(this.jsx).isInvalid) {
-      if (renderings < 1)
+    if (cacheof(this.jsx).isInvalid)
         refresh(nextRevision);
-      else
-        setTimeout(refresh, 0, nextRevision);
-    }
   }
 }
 
