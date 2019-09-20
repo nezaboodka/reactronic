af9a8dad7cefc37741d647771fa530b2a8ada94c
Yury Chetyrko <ychetyrko@gmail.com>
2019-09-20 08:40:21 +0300

    Added concepts.tsx as the picture source

---
+++ samples/concepts.tsx
@@ -0,0 +1,41 @@
+// The below copyright notice and the license permission notice
+// shall be included in all copies or substantial portions.
+// Copyright (C) 2017-2019 Yury Chetyrko <ychetyrko@gmail.com>
+// License: https://raw.githubusercontent.com/nezaboodka/reactronic/master/LICENSE
+
+import * as React from 'react';
+import { stateful, transaction, cached, trigger, statusof } from 'reactronic';
+
+@stateful
+export class Model {
+  // state
+  url: string = "https://nezaboodka.com";
+  content: string = "";
+  timestamp: number = Date.now();
+
+  @transaction
+  async goto(url: string) {
+    this.url = url;
+    const response = await fetch(url);
+    this.content = await response.text();
+    this.timestamp = Date.now();
+  }
+}
+
+export class View extends React.Component<Model> {
+  @cached
+  render() {
+    return (
+      <div>
+        <div>{this.props.url}</div>
+        <div>{this.props.content}</div>
+      </div>
+    );
+  }
+
+  @trigger
+  autorefresh() {
+    if (statusof(this.render).isInvalid)
+      this.setState({}); // refresh
+  }
+}
