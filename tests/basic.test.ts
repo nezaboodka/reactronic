a38b1d9e789d186260f619012cdc2e32c470fe61
Yury Chetyrko <ychetyrko@gmail.com>
2019-10-06 15:07:01 +0300

    Fixed tests

--- tests/basic.test.ts
+++ tests/basic.test.ts
@@ -30,7 +30,7 @@ test("basic", t => {
   const app = Transaction.run("app", () => new DemoView(new DemoModel()));
   try {
     t.is(app.model.methodOfStatefulBase(), "methodOfStatefulBase");
-    t.throws(() => console.log(app.model.unassigned), "unassigned properties are not supported: v103t110#21 DemoModel.unassigned");
+    t.throws(() => console.log(app.model.unassigned), "unassigned properties are not supported: v102t109#21 DemoModel.unassigned");
     t.notThrows(() => DemoView.test());
     // t.assert(app.model.title.startsWith("demo -")); // check that DemoModel.normalizeTitle works
     const rendering = cacheof(app.render);
