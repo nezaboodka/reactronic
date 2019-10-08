9f09af447f3984f3bc5d4d0177bc28e128361e93
Yury Chetyrko <ychetyrko@gmail.com>
2019-10-08 11:27:16 +0300

    Code structure

--- source/api.ts
+++ source/api.ts
@@ -5,7 +5,7 @@
 
 export { all, sleep } from './util/all'
 export { Options, Kind, Reentrance, Trace } from './Options'
-export { Reactronic } from './Rx'
+export { Reactronic } from './Reactronic'
 export { Stateful } from './core/Hooks'
 export { stateless, stateful, transaction, trigger, cached, latency, reentrance, cachedArgs, monitor, trace } from './Options.decorators'
 export { Transaction } from './Transaction'
