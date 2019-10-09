22313881a175994094e092335538a8ee2e8d9480
Yury Chetyrko <ychetyrko@gmail.com>
2019-10-09 11:04:25 +0300

    Cosmetics

--- source/Action.ts
+++ source/Action.ts
@@ -8,9 +8,10 @@ import { Trace } from './Options'
 import { Transaction } from './core/Transaction'
 
 export abstract class Action {
+  static get current(): Action { return Transaction.current }
+
   abstract readonly id: number;
   abstract readonly hint: string;
-
   abstract run<T>(func: F<T>, ...args: any[]): T;
   abstract inspect<T>(func: F<T>, ...args: any[]): T;
   abstract apply(): void;
@@ -21,7 +22,6 @@ export abstract class Action {
   abstract isFinished(): boolean;
   abstract async whenFinished(includingReaction: boolean): Promise<void>;
 
-  static get current(): Action { return Transaction.current }
   static create(hint: string): Action { return new Transaction(hint) }
   static run<T>(hint: string, func: F<T>, ...args: any[]): T { return Transaction.run<T>(hint, func, ...args) }
   static runEx<T>(hint: string, spawn: boolean, sidebyside: boolean, trace: Partial<Trace> | undefined, token: any, func: F<T>, ...args: any[]): T { return Transaction.runEx<T>(hint, spawn, sidebyside, trace, token, func, ...args) }
