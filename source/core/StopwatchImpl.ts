61dd38beec70a2aa78f8fd779debff91eb72d68c
Yury Chetyrko <ychetyrko@gmail.com>
2019-10-10 11:40:41 +0300

    Stopwatch.delay

--- source/core/StopwatchImpl.ts
+++ source/core/StopwatchImpl.ts
@@ -8,17 +8,17 @@ import { Action } from '../Action'
 import { Stopwatch, Worker } from '../Stopwatch'
 
 export class StopwatchImpl extends Stopwatch {
-  private timeout?: NodeJS.Timeout = undefined
-  interval?: number = undefined
+  private timer?: NodeJS.Timeout = undefined
+  delay?: number = undefined
   busy: boolean = false
   count: number = 0
   workers = new Set<Worker>()
   ticks: number = 0
 
   enter(worker: Worker): void {
-    if (this.timeout !== undefined)
-      clearTimeout(this.timeout) // yes, on each enter
-    this.timeout = undefined
+    if (this.timer !== undefined)
+      clearTimeout(this.timer) // yes, on each enter
+    this.timer = undefined
     if (this.count === 0)
       this.busy = true
     this.count++
@@ -29,16 +29,16 @@ export class StopwatchImpl extends Stopwatch {
     this.workers.delete(worker)
     this.count--
     if (this.count === 0) {
-      if (this.interval === undefined)
+      if (this.delay === undefined)
         this.reset()
       else
-        this.timeout = setTimeout(() => this.reset(), this.interval)
+        this.timer = setTimeout(() => this.reset(), this.delay)
     }
   }
 
   private reset(): void {
     this.busy = false
-    this.timeout = undefined
+    this.timer = undefined
     this.ticks = 0
   }
 
@@ -50,14 +50,14 @@ export class StopwatchImpl extends Stopwatch {
     m.leave(worker)
   }
 
-  static create(hint?: string, interval?: number): StopwatchImpl {
-    return Action.run("Stopwatch.create", StopwatchImpl.createFunc, hint, interval)
+  static create(hint?: string, delay?: number): StopwatchImpl {
+    return Action.run("Stopwatch.create", StopwatchImpl.createFunc, hint, delay)
   }
 
-  private static createFunc(hint?: string, interval?: number): StopwatchImpl {
+  private static createFunc(hint?: string, delay?: number): StopwatchImpl {
     const m = new StopwatchImpl()
     Hint.setHint(m, hint)
-    m.interval = interval
+    m.delay = delay
     return m
   }
 }
