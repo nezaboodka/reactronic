395b19ff8505c2dda0e1f28037ad840105e6451a
Yury Chetyrko <ychetyrko@gmail.com>
2019-10-10 11:49:48 +0300

    Progress on stopwatch

--- source/core/StopwatchImpl.ts
+++ source/core/StopwatchImpl.ts
@@ -9,7 +9,7 @@ import { Action } from '../Action'
 import { Stopwatch, Worker } from '../Stopwatch'
 
 export class StopwatchImpl extends Stopwatch {
-  private timer?: NodeJS.Timeout = undefined
+  private timeout: any = undefined
   delay?: number = undefined
   busy: boolean = false
   count: number = 0
@@ -17,9 +17,7 @@ export class StopwatchImpl extends Stopwatch {
   ticks: number = 0
 
   enter(worker: Worker): void {
-    if (this.timer !== undefined)
-      clearTimeout(this.timer) // yes, on each enter
-    this.timer = undefined
+    this.timeout = clear(this.timeout) // yes, on each enter
     if (this.count === 0)
       this.busy = true
     this.count++
@@ -33,7 +31,7 @@ export class StopwatchImpl extends Stopwatch {
       if (this.delay === undefined)
         this.reset()
       else
-        this.timer = setTimeout(() => this.reset(), this.delay)
+        this.timeout = setTimeout(() => this.reset(), this.delay)
     }
   }
 
@@ -43,16 +41,8 @@ export class StopwatchImpl extends Stopwatch {
     this.busy = false
     // this.count = 0
     // this.workers.clear()
-    this.timer = undefined
+    this.timeout = clear(this.timeout)
     this.ticks = 0
-  }
-
-  static enter(m: Stopwatch, worker: Worker): void {
-    m.enter(worker)
-  }
-
-  static leave(m: Stopwatch, worker: Worker): void {
-    m.leave(worker)
   }
 
   static create(hint?: string, delay?: number): StopwatchImpl {
@@ -65,4 +55,17 @@ export class StopwatchImpl extends Stopwatch {
     m.delay = delay
     return m
   }
+
+  static enter(m: Stopwatch, worker: Worker): void {
+    m.enter(worker)
+  }
+
+  static leave(m: Stopwatch, worker: Worker): void {
+    m.leave(worker)
+  }
+}
+
+function clear(timeout: any): undefined {
+  clearTimeout(timeout)
+  return undefined
 }
