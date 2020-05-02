4210672a68be30288baed52572b1473b9066d8e2
Yury Chetyrko <ychetyrko@gmail.com>
2020-05-02 12:41:23 +0300

    Rename back

--- source/api.ts
+++ source/api.ts
@@ -8,7 +8,7 @@ export { Options, Kind, Reentrance, LoggingOptions, ProfilingOptions, LogLevel }
 export { Stateful } from './impl/Hooks'
 export { Reactronic, getCachedAndRevalidate, nonreactive, isolated, state, stateless,
   transaction, trigger, cached, priority, noSideEffects, sensitiveArgs, throttling,
-  reentrance, monitor, logging } from './Rx'
+  reentrance, monitor, logging } from './Reactronic'
 export { Transaction } from './Transaction'
 export { Monitor, Worker } from './Monitor'
 export { Cache } from './Cache'
