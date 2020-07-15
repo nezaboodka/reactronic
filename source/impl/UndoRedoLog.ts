20b176358fc01cca9c806fad151dadfb6094081f
Yury Chetyrko <ychetyrko@gmail.com>
2020-07-15 09:42:59 +0300

    WIP: UndoRedoLog

--- source/impl/UndoRedoLog.ts
+++ source/impl/UndoRedoLog.ts
@@ -3,16 +3,13 @@
 // Copyright (C) 2016-2020 Yury Chetyrko <ychetyrko@gmail.com>
 // License: https://raw.githubusercontent.com/nezaboodka/reactronic/master/LICENSE
 
-import { misuse } from '../util/Dbg'
 import { Stateful } from './Hooks'
 import { Transaction } from '../Transaction'
-
-// UndoRedoLog
 
 export class UndoRedoLog extends Stateful {
   private _capacity: number = 5
   private _items: Transaction[] = []
-  private _position: number = 0 // points right after the latest item
+  private _position: number = 0
 
   get capacity(): number { return this._capacity }
   set capacity(value: number) { this._capacity = value; if (value < this._items.length) this._items.splice(0, this._items.length - value) }
@@ -21,11 +18,13 @@ export class UndoRedoLog extends Stateful {
   get canRedo(): boolean { return this._position < this._items.length }
 
   remember(t: Transaction): void {
-    if (this._items.length >= this._capacity)
-      this._items.shift()
-    else
-      this._items.splice(this._position)
-    this._items.push(t)
+    Transaction.runAs('UndoRedeLog.remember', true, undefined, undefined, () => {
+      if (this._items.length >= this._capacity)
+        this._items.shift()
+      else
+        this._items.splice(this._position)
+      this._items.push(t)
+    })
   }
 
   undo(count: number = 1): void {
@@ -42,6 +41,15 @@ export class UndoRedoLog extends Stateful {
   }
 
   redo(count: number = 1): void {
-    throw misuse('not implemented')
+    let i: number = this._position
+    Transaction.run('redo', () => {
+      while (i < this._items.length && count > 0) {
+        const t: Transaction = this._items[i]
+        t.redo()
+        i++
+        count--
+      }
+    })
+    this._position = i
   }
 }
