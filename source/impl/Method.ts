b084b2dadde75b3c33608374ad2ae3bc5419c8a2
Yury Chetyrko <ychetyrko@gmail.com>
2020-11-17 10:18:13 +0300

    Cosmetics

--- source/impl/Method.ts
+++ source/impl/Method.ts
@@ -588,18 +588,18 @@ class CallResult extends Observable implements Observer {
           prev.observers.forEach(c => c.invalidateDueTo(prev, cause, timestamp, triggers))
       }
     }
-    const cache = r.data[m]
-    if (cache instanceof CallResult && cache.record === r) {
+    const observable = r.data[m]
+    if (observable instanceof CallResult && observable.record === r) {
       if (unsubscribe)
-        cache.unsubscribeFromAll()
+        observable.unsubscribeFromAll()
       // Clear recomputing status of previous cached result
       // const prev = cache.record.prev.record.data[m]
       // if (prev instanceof CallResult && prev.revalidation === cache)
       //   prev.revalidation = undefined
       // Performance tracking
       if (Hooks.repetitiveReadWarningThreshold < Number.MAX_SAFE_INTEGER) {
-        cache.observables.forEach((hint, value) => {
-          if (hint.times > Hooks.repetitiveReadWarningThreshold) Dbg.log('', '[!]', `${cache.hint()} uses ${Hints.record(hint.record, hint.member)} ${hint.times} times (consider remembering it in a local variable)`, 0, ' *** WARNING ***')
+        observable.observables.forEach((hint, value) => {
+          if (hint.times > Hooks.repetitiveReadWarningThreshold) Dbg.log('', '[!]', `${observable.hint()} uses ${Hints.record(hint.record, hint.member)} ${hint.times} times (consider remembering it in a local variable)`, 0, ' *** WARNING ***')
         })
       }
     }
