21a45cdc98252d730140dcd934bd93db10b0dbc2
Yury Chetyrko <ychetyrko@gmail.com>
2022-05-14 13:22:58 +0300

    WIP: ReactiveArray

--- source/impl/Hooks.ts
+++ source/impl/Hooks.ts
@@ -6,6 +6,7 @@
 // automatically licensed under the license referred above.
 
 import { UNDEF, F } from '../util/Utils'
+import { Sealant } from '../util/Sealant'
 import { Log, misuse } from '../util/Dbg'
 import { MemberOptions, Kind, Reentrance } from '../Options'
 import { LoggingOptions, ProfilingOptions } from '../Logging'
@@ -39,21 +40,21 @@ export class ReactiveArray<T> extends ReactiveObject {
   get length(): number { return this.a.length }
   set length(n: number) { this.a.length = n }
   get(n: number): T { return this.a[n] }
-  set(n: number, item: T): void { this.a[n] = item }
+  set(n: number, item: T): void { this.mutable[n] = item }
   toString(): string { return this.a.toString() }
   toLocaleString(): string { return this.a.toLocaleString() }
-  pop(): T | undefined { return this.a.pop() }
-  push(...items: T[]): number { return this.push(...items) }
+  pop(): T | undefined { return this.mutable.pop() }
+  push(...items: T[]): number { return this.mutable.push(...items) }
   concat(...items: (T | ConcatArray<T>)[]): T[]
-  concat(...items: ConcatArray<T>[]): T[] { return this.concat(...items) }
+  concat(...items: ConcatArray<T>[]): T[] { return this.a.concat(...items) }
   join(separator?: string): string { return this.a.join(separator) }
-  reverse(): T[] { return this.a.reverse() }
-  shift(): T | undefined { return this.a.shift() }
+  reverse(): T[] { return this.mutable.reverse() }
+  shift(): T | undefined { return this.mutable.shift() }
   slice(start?: number, end?: number): T[] { return this.a.slice(start, end) }
-  sort(compareFn?: (a: T, b: T) => number): this { this.a.sort(compareFn); return this }
+  sort(compareFn?: (a: T, b: T) => number): this { this.mutable.sort(compareFn); return this }
   splice(start: number, deleteCount?: number): T[]
-  splice(start: number, deleteCount: number, ...items: T[]): T[] { return this.a.splice(start, deleteCount, ...items) }
-  unshift(...items: T[]): number { return this.a.unshift(...items) }
+  splice(start: number, deleteCount: number, ...items: T[]): T[] { return this.mutable.splice(start, deleteCount, ...items) }
+  unshift(...items: T[]): number { return this.mutable.unshift(...items) }
   indexOf(searchElement: T, fromIndex?: number): number { return this.a.indexOf(searchElement, fromIndex) }
   lastIndexOf(searchElement: T, fromIndex?: number): number { return this.a.lastIndexOf(searchElement, fromIndex) }
   every(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): boolean
@@ -67,6 +68,13 @@ export class ReactiveArray<T> extends ReactiveObject {
   reduce<U>(callbackfn: (previousValue: U, currentValue: T, currentIndex: number, array: T[]) => U, initialValue: U): U { return this.a.reduce(callbackfn, initialValue) }
   reduceRight(callbackfn: (previousValue: T, currentValue: T, currentIndex: number, array: T[]) => T, initialValue: T): T
   reduceRight<U>(callbackfn: (previousValue: U, currentValue: T, currentIndex: number, array: T[]) => U, initialValue: U): U { return this.a.reduceRight(callbackfn, initialValue) }
+
+  private get mutable(): Array<T> {
+    const createCopy = (this.a as any)[Sealant.CreateCopy]
+    if (createCopy)
+      return this.a = createCopy.call(this.a)
+    return this.a
+  }
 }
 
 // Options
