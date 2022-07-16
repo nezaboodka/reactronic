da5ff6466718183b945531b477c5d2d4b9684af4
Yury Chetyrko <ychetyrko@gmail.com>
2022-07-16 20:39:18 +0300

    Moved Collection from Reactronic-Dom to Reactronic

---
+++ source/util/Collection.ts
@@ -0,0 +1,362 @@
+// The below copyright notice and the license permission notice
+// shall be included in all copies or substantial portions.
+// Copyright (C) 2016-2022 Yury Chetyrko <ychetyrko@gmail.com>
+// License: https://raw.githubusercontent.com/nezaboodka/reactronic/master/LICENSE
+// By contributing, you agree that your contributions will be
+// automatically licensed under the license referred above.
+
+export type GetItemKey<T = unknown> = (item: T) => string | undefined
+
+export interface CollectionReader<T> {
+  // readonly getKey: GetKey<T>
+  readonly strict: boolean
+  readonly count: number
+  readonly addedCount: number
+  readonly removedCount: number
+  readonly isMergeInProgress: boolean
+
+  lookup(key: string): Item<T> | undefined
+  claim(key: string): Item<T> | undefined
+  add(self: T): Item<T>
+  remove(item: Item<T>): void
+  move(item: Item<T>, after: Item<T>): void
+  beginMerge(): void
+  endMerge(error?: unknown): void
+  resetAddedAndRemovedLists(): void
+
+  items(): Generator<Item<T>>
+  addedItems(reset?: boolean): Generator<Item<T>>
+  removedItems(reset?: boolean): Generator<Item<T>>
+  isAdded(item: Item<T>): boolean
+  isMoved(item: Item<T>): boolean
+  isRemoved(item: Item<T>): boolean
+  isCurrent(item: Item<T>): boolean
+}
+
+export interface Item<T> {
+  readonly self: T
+  // readonly next?: Item<T>
+  readonly prev?: Item<T> // TODO: hide
+  aux?: Item<T> // TODO: hide
+}
+
+export class Collection<T> implements CollectionReader<T> {
+  readonly strict: boolean
+  readonly getKey: GetItemKey<T>
+  private map: Map<string | undefined, ItemImpl<T>>
+  private tag: number
+  private current: ItemChain<T>
+  private added: ItemChain<T>
+  private removed: ItemChain<T>
+  private lastNotFoundKey: string | undefined
+  private strictNextItem?: ItemImpl<T>
+
+  constructor(strict: boolean, getKey: GetItemKey<T>) {
+    this.strict = strict
+    this.getKey = getKey
+    this.map = new Map<string | undefined, ItemImpl<T>>()
+    this.tag = ~0
+    this.current = new ItemChain<T>()
+    this.added = new ItemChain<T>()
+    this.removed = new ItemChain<T>()
+    this.lastNotFoundKey = undefined
+    this.strictNextItem = undefined
+  }
+
+  get count(): number {
+    return this.current.count
+  }
+
+  get addedCount(): number {
+    return this.added.count
+  }
+
+  get removedCount(): number {
+    return this.removed.count
+  }
+
+  get isMergeInProgress(): boolean {
+    return this.tag > 0
+  }
+
+  lookup(key: string | undefined): Item<T> | undefined {
+    let result: Item<T> | undefined = undefined
+    if (key !== undefined && key !== this.lastNotFoundKey) {
+      result = this.map.get(key)
+      if (result) {
+        if (this.getKey(result.self) !== key) {
+          this.lastNotFoundKey = key
+          result = undefined
+        }
+      }
+      else
+        this.lastNotFoundKey = key
+    }
+    return result
+  }
+
+  claim(key: string): Item<T> | undefined {
+    const tag = this.tag
+    if (tag < 0)
+      throw new Error('merge is not in progress')
+    let item = this.strictNextItem
+    if (key !== (item ? this.getKey(item.self) : undefined))
+      item = this.lookup(key) as ItemImpl<T> | undefined
+    if (item) {
+      if (item.tag === tag)
+        throw new Error(`duplicate item: ${key}`)
+      item.tag = tag
+      if (this.strict && item !== this.strictNextItem)
+        item.status = tag // IsAdded=false, IsMoved=true
+      this.strictNextItem = item.next
+      this.removed.exclude(item)
+      this.current.include(item)
+    }
+    return item
+  }
+
+  add(self: T): Item<T> {
+    const key = this.getKey(self)
+    if (this.lookup(key) !== undefined)
+      throw new Error(`key is already in use: ${key}`)
+    let tag = this.tag
+    if (tag < 0) { // merge is not in progress
+      tag = ~this.tag + 1
+      this.tag = ~tag // one item merge cycle
+    }
+    const item = new ItemImpl<T>(self, tag)
+    this.map.set(key, item)
+    this.lastNotFoundKey = undefined
+    this.strictNextItem = undefined
+    this.current.include(item)
+    this.added.aux(item)
+    return item
+  }
+
+  remove(item: Item<T>): void {
+    const t = item as ItemImpl<T>
+    if (!this.isRemoved(t)) {
+      this.current.exclude(t)
+      this.removed.include(t)
+      t.tag--
+    }
+  }
+
+  move(item: Item<T>, after: Item<T>): void {
+    throw new Error('not implemented')
+  }
+
+  beginMerge(): void {
+    if (this.isMergeInProgress)
+      throw new Error('merge is in progress already')
+    this.tag = ~this.tag + 1
+    this.strictNextItem = this.current.first
+    this.removed.grab(this.current, false)
+    this.added.reset()
+  }
+
+  endMerge(error?: unknown): void {
+    if (!this.isMergeInProgress)
+      throw new Error('merge is ended already')
+    this.tag = ~this.tag
+    if (error === undefined) {
+      const currentCount = this.current.count
+      if (currentCount > 0) {
+        const getKey = this.getKey
+        if (currentCount > this.removed.count) { // it should be faster to delete vanished items
+          const map = this.map
+          for (const x of this.removed.items())
+            map.delete(getKey(x.self))
+        }
+        else { // it should be faster to recreate map using current items
+          const map = this.map = new Map<string | undefined, ItemImpl<T>>()
+          for (const x of this.current.items())
+            map.set(getKey(x.self), x)
+        }
+      }
+      else // just create new empty map
+        this.map = new Map<string | undefined, ItemImpl<T>>()
+    }
+    else {
+      this.current.grab(this.removed, true)
+      const getKey = this.getKey
+      for (const x of this.added.itemsViaAux()) {
+        this.map.delete(getKey(x.self))
+        this.current.exclude(x)
+      }
+      this.added.reset()
+    }
+  }
+
+  resetAddedAndRemovedLists(): void {
+    this.removed.reset()
+    this.added.reset()
+  }
+
+  *items(): Generator<Item<T>> {
+    let x = this.current.first
+    while (x !== undefined) {
+      const next = x.next
+      yield x
+      x = next
+    }
+  }
+
+  *addedItems(reset?: boolean): Generator<Item<T>> {
+    let x = this.added.first
+    while (x !== undefined) {
+      const next = x.aux
+      if (!this.isRemoved(x))
+        yield x
+      x = next
+    }
+    if (reset)
+      this.added.reset()
+  }
+
+  *removedItems(reset?: boolean): Generator<Item<T>> {
+    let x = this.removed.first
+    while (x !== undefined) {
+      const next = x.next
+      yield x
+      x = next
+    }
+    if (reset)
+      this.removed.reset()
+  }
+
+  isAdded(item: Item<T>): boolean {
+    const t = item as ItemImpl<T>
+    let tag = this.tag
+    if (tag < 0)
+      tag = ~tag
+    return t.status === ~tag && t.tag > 0
+  }
+
+  isMoved(item: Item<T>): boolean {
+    const t = item as ItemImpl<T>
+    let tag = this.tag
+    if (tag < 0)
+      tag = ~tag
+    return t.status === tag && t.tag > 0
+  }
+
+  isRemoved(item: Item<T>): boolean {
+    const t = item as ItemImpl<T>
+    const tag = this.tag
+    return tag > 0 ? t.tag < tag : t.tag < tag - 1
+  }
+
+  isCurrent(item: Item<T>): boolean {
+    const t = item as ItemImpl<T>
+    return t.tag === this.tag
+  }
+
+  markAsMoved(item: Item<T>): void {
+    const t = item as ItemImpl<T>
+    if (t.tag > 0) // if not removed, > is intentional
+      t.status = t.tag
+  }
+
+  static createItem<T>(self: T): Item<T> {
+    return new ItemImpl(self, 0)
+  }
+}
+
+class ItemImpl<T> implements Item<T> {
+  readonly self: T
+  tag: number
+  status: number
+  next?: ItemImpl<T>
+  prev?: ItemImpl<T>
+  aux?: ItemImpl<T>
+
+  constructor(self: T, tag: number) {
+    this.self = self
+    this.tag = tag
+    this.status = ~tag // isAdded=true
+    this.next = undefined
+    this.prev = undefined
+    this.aux = undefined
+  }
+}
+
+class ItemChain<T> {
+  count: number = 0
+  first?: ItemImpl<T> = undefined
+  last?: ItemImpl<T> = undefined
+
+  public *items(): Generator<ItemImpl<T>> {
+    let x = this.first
+    while (x !== undefined) {
+      const next = x.next
+      yield x
+      x = next
+    }
+  }
+
+  public *itemsViaAux(): Generator<ItemImpl<T>> {
+    let x = this.first
+    while (x !== undefined) {
+      const next = x.aux
+      yield x
+      x = next
+    }
+  }
+
+  reset(): void {
+    this.count = 0
+    this.first = undefined
+    this.last = undefined
+  }
+
+  grab(from: ItemChain<T>, join: boolean): void {
+    const head = from.first
+    if (join && head) {
+      const last = this.last
+      head.prev = last
+      if (last)
+        this.last = last.next = head
+      else
+        this.first = this.last = head
+      this.count += from.count
+    }
+    else {
+      this.count = from.count
+      this.first = head
+      this.last = from.last
+    }
+    from.reset()
+  }
+
+  include(item: ItemImpl<T>): void {
+    const last = this.last
+    item.prev = last
+    item.next = undefined
+    if (last)
+      this.last = last.next = item
+    else
+      this.first = this.last = item
+    this.count++
+  }
+
+  exclude(item: ItemImpl<T>): void {
+    if (item.prev !== undefined)
+      item.prev.next = item.next
+    if (item.next !== undefined)
+      item.next.prev = item.prev
+    if (item === this.first)
+      this.first = item.next
+    this.count--
+  }
+
+  aux(item: ItemImpl<T>): void {
+    item.aux = undefined
+    const last = this.last
+    if (last)
+      this.last = last.aux = item
+    else
+      this.first = this.last = item
+    this.count++
+  }
+}
---
+++ test/collection.test.ts
@@ -0,0 +1,91 @@
+﻿// The below copyright notice and the license permission notice
+// shall be included in all copies or substantial portions.
+// Copyright (C) 2016-2022 Yury Chetyrko <ychetyrko@gmail.com>
+// License: https://raw.githubusercontent.com/nezaboodka/reactronic/master/LICENSE
+// By contributing, you agree that your contributions will be
+// automatically licensed under the license referred above.
+
+import test from 'ava'
+import { Collection, Item } from '../source/util/Collection'
+
+test('collection', t => {
+  const etalon1 = ['Hello', 'Welcome', 'Bye', 'End']
+  const etalon2 = ['Added1', 'Bye', 'End', 'Added2', 'Hello', 'Added3']
+  const etalon2a = ['Hello', 'Bye', 'End', 'Added1', 'Added2', 'Added3']
+
+  // Basic
+  const list = new Collection<string>(true, s => s)
+  for (const x of etalon1)
+    list.add(x)
+
+  t.is(list.count, 4)
+  t.is(list.addedCount, 4)
+  t.is(list.removedCount, 0)
+  t.true(compare(list.items(), etalon1))
+
+  // Merge etalon2 with etalon1
+  list.beginMerge()
+  for (const x of etalon2)
+    if (!list.claim(x))
+      list.add(x)
+  list.endMerge()
+
+  t.is(list.count, 6)
+  t.is(list.removedCount, 1)
+  t.is(list.addedCount, 3)
+  t.true(compare(list.items(), etalon2))
+  t.true(compare(list.removedItems(), ['Welcome']))
+  t.true(compare(list.addedItems(), ['Added1', 'Added2', 'Added3']))
+  t.is(list.removedCount, 1)
+  t.is(list.addedCount, 3)
+  list.resetAddedAndRemovedLists()
+  t.is(list.removedCount, 0)
+  t.is(list.addedCount, 0)
+  t.true(list.isAdded(list.lookup('Added1')!))
+  t.true(list.isAdded(list.lookup('Added2')!))
+  t.true(list.isAdded(list.lookup('Added3')!))
+  t.true(list.isMoved(list.lookup('Bye')!))
+  t.false(list.isMoved(list.lookup('End')!))
+  t.true(list.isMoved(list.lookup('Hello')!))
+
+  // Merge back, but with error
+  list.beginMerge()
+  for (const x of etalon1)
+    if (!list.claim(x))
+      list.add(x)
+  t.is(list.count, 4)
+  t.is(list.removedCount, 3)
+  t.is(list.addedCount, 1)
+  list.endMerge('error')
+  t.is(list.count, 6)
+  t.is(list.removedCount, 0)
+  t.is(list.addedCount, 0)
+  t.true(compare(list.items(), etalon2a))
+
+  // Merge back again (success)
+  list.beginMerge()
+  for (const x of etalon1)
+    if (!list.claim(x))
+      list.add(x)
+  t.is(list.count, 4)
+  t.is(list.removedCount, 3)
+  t.is(list.addedCount, 1)
+  list.endMerge()
+  t.is(list.count, 4)
+  t.is(list.removedCount, 3)
+  t.is(list.addedCount, 1)
+  t.true(compare(list.items(), etalon1))
+})
+
+function compare(list: Generator<Item<string>>, array: Array<string>): boolean {
+  let result = true
+  let i = 0
+  for (const item of list) {
+    if (item.self !== array[i]) {
+      result = false
+      break
+    }
+    i++
+  }
+  return result && i === array.length
+}
