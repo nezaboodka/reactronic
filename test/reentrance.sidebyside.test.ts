ee93893e2e540f596b654064f1a4ef112b69109b
Yury Chetyrko <ychetyrko@gmail.com>
2023-09-30 09:04:00 +0300

    Copyright

--- test/reentrance.sidebyside.test.ts
+++ test/reentrance.sidebyside.test.ts
@@ -1,6 +1,6 @@
 ﻿// The below copyright notice and the license permission notice
 // shall be included in all copies or substantial portions.
-// Copyright (C) 2016-2022 Nezaboodka Software <contact@nezaboodka.com>
+// Copyright (C) 2016-2023 Nezaboodka Software <contact@nezaboodka.by>
 // License: https://raw.githubusercontent.com/nezaboodka/reactronic/master/LICENSE
 // By contributing, you agree that your contributions will be
 // automatically licensed under the license referred above.
