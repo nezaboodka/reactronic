d909ed749fbb7f07b715c6f45ee0a9bf689d311f
Yury Chetyrko <ychetyrko@gmail.com>
2023-10-07 16:13:20 +0300

    WIP: C# impl

---
+++ dotnet/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+public abstract class User
+{
+  public abstract string Name { get; set; }
+  public abstract string Email { get; set; }
+  public abstract DateTime Birthday { get; set; }
+}
