d9139d511e8186a0edfce45cd162c6d6bdb96cd3
Yury Chetyrko <ychetyrko@gmail.com>
2025-02-07 23:35:24 +0300

    Copyright 2025

--- test/indicator.test.ts
+++ test/indicator.test.ts
@@ -1,6 +1,6 @@
 ﻿// The below copyright notice and the license permission notice
 // shall be included in all copies or substantial portions.
-// Copyright (C) 2016-2024 Nezaboodka Software <contact@nezaboodka.by>
+// Copyright (C) 2016-2025 Nezaboodka Software <contact@nezaboodka.by>
 // License: https://raw.githubusercontent.com/nezaboodka/reactronic/master/LICENSE
 // By contributing, you agree that your contributions will be
 // automatically licensed under the license referred above.
