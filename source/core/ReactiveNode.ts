184abc789b2d3d69d3622df6a51c1a8766881ff6
Yury Chetyrko <ychetyrko@gmail.com>
2025-02-14 11:02:00 +0300

    Copyright

--- source/core/ReactiveNode.ts
+++ source/core/ReactiveNode.ts
@@ -1,6 +1,6 @@
 // The below copyright notice and the license permission notice
 // shall be included in all copies or substantial portions.
-// Copyright (C) 2019-2024 Nezaboodka Software <contact@nezaboodka.com>
+// Copyright (C) 2019-2025 Nezaboodka Software <contact@nezaboodka.com>
 // License: https://raw.githubusercontent.com/nezaboodka/verstak/master/LICENSE
 // By contributing, you agree that your contributions will be
 // automatically licensed under the license referred above.
